0fda9ab6390e684dacf805f1656d1c0d4811ddfa
Alexey <koroza.alexey99@gmail.com>
2022-06-22 19:29:10 +0200

    *

---
+++ basics_oop/src/by/koroza/basics_oop/numberthree/main/Main.java
@@ -0,0 +1,12 @@
+/* Создать класс Календарь с внутренним классом, 
+ * с помощью объектов которого можно хранить информацию о выходных и праздничных днях.
+ * */
+
+package by.koroza.basics_oop.numberthree.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+}
