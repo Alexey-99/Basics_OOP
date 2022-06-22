ab14664a0ba26b2cd2eb7f552fe3235d59cde875
Alexey <koroza.alexey99@gmail.com>
2022-06-22 19:21:42 +0200

    *

---
+++ basics_oop/src/by/koroza/basics_oop/numberone/main/Main.java
@@ -0,0 +1,12 @@
+/* Создать объект класса Текстовый файл, используя классы Файл, Директория. 
+ * Методы: создать, переименовать, вывести на консоль содержимое, дополнить, удалить. 
+ * */
+
+package by.koroza.basics_oop.numberone.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+}
---
+++ basics_oop/src/by/koroza/basics_oop/numbertwo/main/Main.java
@@ -0,0 +1,12 @@
+/* Создать класс Payment с внутренним классом, 
+ * с помощью объектов которого можно сформировать покупку из нескольких товаров.
+ * */
+
+package by.koroza.basics_oop.numbertwo.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+}
