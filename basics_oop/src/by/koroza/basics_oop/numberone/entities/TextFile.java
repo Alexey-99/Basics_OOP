99c5e47b9a18f78e33898fc1213f41277f5870a3
Alexey <koroza.alexey99@gmail.com>
2022-06-23 17:51:21 +0200

    *

--- basics_oop/src/by/koroza/basics_oop/numberone/entities/TextFile.java
+++ basics_oop/src/by/koroza/basics_oop/numberone/entities/TextFile.java
@@ -1,12 +1,35 @@
 package by.koroza.basics_oop.numberone.entities;
 
 public class TextFile {
+	private String title;
+	private String content;
+
+	public TextFile() {
+
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
 
 	@Override
 	public int hashCode() {
 		int result = 31;
 		int prime = 1;
-		result = result * prime;
+		result = result * prime + (title != null ? title.hashCode() : 1);
+		result = result * prime + (content != null ? content.hashCode() : 1);
 		return result;
 	}
 
@@ -22,6 +45,20 @@ public class TextFile {
 			return false;
 		}
 		TextFile textFile = (TextFile) object;
+		if (title == null) {
+			if (textFile.title != null) {
+				return false;
+			}
+		} else if (!title.equals(textFile.title)) {
+			return false;
+		}
+		if (content == null) {
+			if (textFile.content != null) {
+				return false;
+			}
+		} else if (!content.equals(textFile.content)) {
+			return false;
+		}
 		return true;
 	}
 
