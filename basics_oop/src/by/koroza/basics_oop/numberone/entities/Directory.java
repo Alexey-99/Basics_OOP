f8aa1d5b09c2e8fae0f54f390a86b50be58d9c69
Alexey <koroza.alexey99@gmail.com>
2022-06-23 14:34:39 +0200

    *

--- basics_oop/src/by/koroza/basics_oop/numberone/entities/Directory.java
+++ basics_oop/src/by/koroza/basics_oop/numberone/entities/Directory.java
@@ -1,5 +1,15 @@
 package by.koroza.basics_oop.numberone.entities;
 
 public class Directory {
+	private TextFile[] files;
 
+	public TextFile[] getFiles() {
+		return files;
+	}
+
+	public void setFiles(TextFile[] files) {
+		this.files = files;
+	}
+	
+	
 }
