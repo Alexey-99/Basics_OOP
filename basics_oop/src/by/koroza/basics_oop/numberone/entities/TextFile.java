9914e7dd32c1fa3171a87b2c87a53686cc75d1e6
Alexey <koroza.alexey99@gmail.com>
2022-06-23 17:39:32 +0200

    *

--- basics_oop/src/by/koroza/basics_oop/numberone/entities/TextFile.java
+++ basics_oop/src/by/koroza/basics_oop/numberone/entities/TextFile.java
@@ -2,5 +2,32 @@ package by.koroza.basics_oop.numberone.entities;
 
 public class TextFile {
 
-	
+	@Override
+	public int hashCode() {
+		int result = 31;
+		int prime = 1;
+		result = result * prime;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+		if (object == null) {
+			return false;
+		}
+		if (!getClass().equals(object.getClass())) {
+			return false;
+		}
+		TextFile textFile = (TextFile) object;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		return builder.toString();
+	}
 }
