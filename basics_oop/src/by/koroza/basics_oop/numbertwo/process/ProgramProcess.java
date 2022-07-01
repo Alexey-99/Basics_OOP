f0728cdcbd7ba1c53ca271e5450a33fdbb30622b
Alexey <koroza.alexey99@gmail.com>
2022-07-01 18:12:47 +0200

    *

--- basics_oop/src/by/koroza/basics_oop/numbertwo/process/ProgramProcess.java
+++ basics_oop/src/by/koroza/basics_oop/numbertwo/process/ProgramProcess.java
@@ -10,19 +10,20 @@ public class ProgramProcess {
 	private static final String ENTER_LAST_NAME = "Enter last name (Example: Koroza)";
 	private static final String ENTER_FIRST_NAME = "Enter first name (Example: Alexey)";
 	private static final String ENTER_PATRONYMIC = "Enter patronymic (Example: Mikhailovich)";
-
-	private static final String DO_YOU_WANT_ADD_PRODUCT = " do you want to add a product? Enter Yes - 0, No - 1 ";
+	private static final String DO_YOU_WANT_ADD_PRODUCT = "do you want to add a product? Enter Yes - 0, No - 1 ";
+	private static final String SPACE = " ";
+	private static final String YES = "0";
+	private static final String ENTER_PRODUCT_NAME = "Enter product name.";
 
 	public static void programProcess() {
-		Person solder = createSolder();
+		Person salesman = createSalesman();
 		Person costomer = createCostomer();
 	}
 
-	private static Person createSolder() {
-		Person solder = new Person(enterLastName(), enterFirstName(), enterPatronymic());
-		addProduct(solder);
-		// solder.addProduct(new Product("A", 5.99));
-		return solder;
+	private static Person createSalesman() {
+		Person salesman = new Person(enterLastName(), enterFirstName(), enterPatronymic());
+		addProduct(salesman);
+		return salesman;
 	}
 
 	private static Person createCostomer() {
@@ -64,11 +65,45 @@ public class ProgramProcess {
 	}
 
 	private static void addProduct(Person person) {
+		String answer = "";
+		do {
+			answer = productAdditionConfirmation(person);
+			if (answer.equals(YES)) {
+				person.addProduct(createProduct());
+			}
+		} while (answer.equals(YES));
+
+	}
+
+	@SuppressWarnings("resource")
+	private static String productAdditionConfirmation(Person person) {
+		StringBuilder builder = new StringBuilder();
+		builder.append(person.getLastName()).append(SPACE);
+		builder.append(person.getFirstName()).append(SPACE);
+		builder.append(person.getPatronymic()).append(SPACE);
+		builder.append(DO_YOU_WANT_ADD_PRODUCT);
 		Scanner scan = new Scanner(System.in);
 		String answer = "";
-		System.out.println(DO_YOU_WANT_ADD_PRODUCT);
+		System.out.println(builder);
 		do {
 			answer = scan.nextLine();
 		} while (Validation.validationAnswerZeroOrOne(answer) == false);
+		return answer;
+	}
+
+	private static Product createProduct() {
+		Product product = new Product(enterProductName(), 1.5);
+		return product;
+	}
+	
+	@SuppressWarnings("resource")
+	private static String enterProductName() {
+		Scanner scan = new Scanner(System.in);
+		String name = "";
+		do {
+			System.out.println(ENTER_PRODUCT_NAME);
+			name = scan.nextLine();
+		} while (Validation.validationPersonName(name) == false);
+		return name;
 	}
 }
