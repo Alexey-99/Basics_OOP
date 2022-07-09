42e176809d1e0ce47f35b6e8830413d2f34cdea6
Alexey <koroza.alexey99@gmail.com>
2022-07-09 17:42:42 +0200

    *

--- basics_oop/src/by/koroza/basics_oop/numbertwo/entity/Person.java
+++ basics_oop/src/by/koroza/basics_oop/numbertwo/entity/Person.java
@@ -7,7 +7,7 @@ public class Person {
 	private String lastName;
 	private String firstName;
 	private String patronymic;
-	private BankAccount[] bankAccounts;
+	private BankAccount bankAccount;
 	private Product[] products;
 	private static final String DOES_NOT_HAVE_PRODUCTS = "Doesn't have products for sale";
 	private static final int ONE_ELEMENT = 1;
@@ -17,8 +17,7 @@ public class Person {
 		this.lastName = lastName;
 		this.firstName = firstName;
 		this.patronymic = patronymic;
-		this.bankAccounts = new BankAccount[1];
-		this.bankAccounts[0] = new BankAccount();
+		this.bankAccount = new BankAccount();
 		this.products = new Product[0];
 	}
 
@@ -54,12 +53,12 @@ public class Person {
 		this.patronymic = patronymic;
 	}
 
-	public BankAccount[] getBankAccounts() {
-		return bankAccounts;
+	public BankAccount getBankAccount() {
+		return bankAccount;
 	}
 
-	public void setBankAccounts(BankAccount[] bankAccounts) {
-		this.bankAccounts = bankAccounts;
+	public void setBankAccount(BankAccount bankAccount) {
+		this.bankAccount = bankAccount;
 	}
 
 	public Product[] getProducts() {
@@ -68,18 +67,6 @@ public class Person {
 
 	public void setProducts(Product[] products) {
 		this.products = products;
-	}
-
-	public void addBankAccount() {
-		BankAccount[] newBankAccounts = new BankAccount[this.bankAccounts.length + ONE_ELEMENT];
-		for (int i = 0; i < newBankAccounts.length; i++) {
-			if (i < this.bankAccounts.length) {
-				newBankAccounts[i] = this.bankAccounts[i];
-			} else if (i == this.bankAccounts.length) {
-				newBankAccounts[i] = new BankAccount();
-			}
-		}
-		this.bankAccounts = newBankAccounts;
 	}
 
 	public void addProduct(Product product) {
@@ -124,10 +111,9 @@ public class Person {
 		result = result * prime + (lastName != null ? lastName.hashCode() : 1);
 		result = result * prime + (firstName != null ? firstName.hashCode() : 1);
 		result = result * prime + (patronymic != null ? patronymic.hashCode() : 1);
-		result = result * prime + (bankAccounts != null ? bankAccounts.hashCode() : 1);
+		result = result * prime + (bankAccount != null ? bankAccount.hashCode() : 1);
 		result = result * prime + (products != null ? products.hashCode() : 1);
-		result = result * prime
-				+ (DOES_NOT_HAVE_PRODUCTS != null ? DOES_NOT_HAVE_PRODUCTS.hashCode() : 1);
+		result = result * prime + (DOES_NOT_HAVE_PRODUCTS != null ? DOES_NOT_HAVE_PRODUCTS.hashCode() : 1);
 		return result;
 	}
 
@@ -167,11 +153,11 @@ public class Person {
 		} else if (!patronymic.equals(person.patronymic)) {
 			return false;
 		}
-		if (bankAccounts == null) {
-			if (person.bankAccounts != null) {
+		if (bankAccount == null) {
+			if (person.bankAccount != null) {
 				return false;
 			}
-		} else if (!bankAccounts.equals(person.bankAccounts)) {
+		} else if (!bankAccount.equals(person.bankAccount)) {
 			return false;
 		}
 		if (products == null) {
--- basics_oop/src/by/koroza/basics_oop/numbertwo/main/process/Payment.java
+++ basics_oop/src/by/koroza/basics_oop/numbertwo/main/process/Payment.java
@@ -2,7 +2,6 @@ package by.koroza.basics_oop.numbertwo.main.process;
 
 import java.util.Scanner;
 
-import by.koroza.basics_oop.numbertwo.entity.BankAccount;
 import by.koroza.basics_oop.numbertwo.entity.Person;
 import by.koroza.basics_oop.numbertwo.entity.Product;
 import by.koroza.basics_oop.numbertwo.validation.Validation;
@@ -10,16 +9,11 @@ import by.koroza.basics_oop.numbertwo.validation.Validation;
 public class Payment {
 	private static final String DOES_NOT_MONEY_IN_BANK_ACCOUNT = "you doesn't have enough moneyin your bank account. ";
 	private static final String WOULD_YOU_WANT_TOP_UP_YOUR_ACCOUNT = "Would you want to top up your account? Enter Yes - 0, No - 1";
-	private static final String DO_YOU_WANT_DELETE_PRODUCT_FROM_RESERVES = "Do you want to delete a product from reserves customer? Yes - 0, No - 1";
 	private static final String SPACE = " ";
 	private static final String YES = "0";
 	private static final String NO = "1";
-	private static final String MESSAGE_ERROW = "You entered incorrectly.";
-	private static final String MESAGE_ENTER_NUMBER_DELESION_PRODUCT = "Enter the number of the product you want to delete.";
 
 	public static void payment(Person salesman, Person customer) {
-		BankAccount[] bankAccountsSalesman = salesman.getBankAccounts();
-		BankAccount[] bankAccountsCustomer = customer.getBankAccounts();
 		double sumPayment = calculationSumPayment(customer.getProducts());
 		checkBalanceCustomer(salesman, customer, sumPayment);
 	}
@@ -33,23 +27,16 @@ public class Payment {
 	}
 
 	private static void checkBalanceCustomer(Person salesman, Person customer, double sumPayment) {
-		if (customer.getBankAccounts().length > 1) {
-
-		} else if (customer.getBankAccounts().length == 1) {
-			if (customer.getBankAccounts()[0].getBalance() < sumPayment) {
-				String answer = enterAnswerOnReplemenishmentBankAccount(customer);
-				if (answer.equals(YES)) {
-					customer.getBankAccounts()[0]
-							.replenishmentBalance(sumPayment - customer.getBankAccounts()[0].getBalance());
-					transferringMoney(salesman, customer, sumPayment);
-				} else if (answer.equals(NO)) {
-					withdrawalReserves(salesman, customer);
-				}
-			} else {
+		if (customer.getBankAccount().getBalance() < sumPayment) {
+			String answer = enterAnswerOnReplemenishmentBankAccount(customer);
+			if (answer.equals(YES)) {
+				customer.getBankAccount().replenishmentBalance(sumPayment - customer.getBankAccount().getBalance());
 				transferringMoney(salesman, customer, sumPayment);
+			} else if (answer.equals(NO)) {
+				withdrawalReserves(salesman, customer);
 			}
-		} else if (customer.getBankAccounts().length == 0) {
-			withdrawalReserves(salesman, customer);
+		} else {
+			transferringMoney(salesman, customer, sumPayment);
 		}
 	}
 
@@ -80,26 +67,6 @@ public class Payment {
 		customer.setProducts(new Product[0]);
 	}
 
-	private static void deleteReserveCustomer(Person customer) {
-		StringBuilder builder = new StringBuilder();
-		builder.append(customer.getLastName()).append(SPACE);
-		builder.append(customer.getFirstName()).append(SPACE);
-		builder.append(customer.getPatronymic()).append(SPACE);
-		builder.append(DO_YOU_WANT_DELETE_PRODUCT_FROM_RESERVES);
-		String answer = enterAnswerYesOrNo(builder);
-		int number = 0;
-		if (answer.equals(YES)) {
-			do {
-				printProducts(customer);
-				number = enterNumberProductToDelete(customer);
-				if (number < customer.getProducts().length) {
-					customer.deleteProduct(customer.getProducts()[number]);
-				}
-			} while (number != customer.getProducts().length);
-
-		}
-	}
-
 	@SuppressWarnings("resource")
 	private static String enterAnswerYesOrNo(StringBuilder builder) {
 		Scanner scan = new Scanner(System.in);
@@ -111,35 +78,8 @@ public class Payment {
 		return answer;
 	}
 
-	private static void printProducts(Person customer) {
-		customer.printProducts();
-		if (customer.getProducts().length > 0) {
-			System.out.println(customer.getProducts().length + " - exit from deletion.");
-		}
-	}
-
 	private static void transferringMoney(Person salesman, Person customer, double sumPayment) {
-		customer.getBankAccounts()[0].setBalance(customer.getBankAccounts()[0].getBalance() - sumPayment);
-		salesman.getBankAccounts()[0].setBalance(salesman.getBankAccounts()[0].getBalance() + sumPayment);
-	}
-
-	@SuppressWarnings("resource")
-	private static int enterNumberProductToDelete(Person customer) {
-		StringBuilder builder = new StringBuilder();
-		builder.append(customer.getLastName()).append(SPACE);
-		builder.append(customer.getFirstName()).append(SPACE);
-		builder.append(customer.getPatronymic()).append(SPACE);
-		builder.append(MESAGE_ENTER_NUMBER_DELESION_PRODUCT);
-		int number = 0;
-		do {
-			System.out.println(builder);
-			Scanner scan = new Scanner(System.in);
-			if (scan.hasNextInt()) {
-				number = scan.nextInt();
-			} else {
-				System.out.println(MESSAGE_ERROW);
-			}
-		} while (Validation.validationEnteredNumberProductForDeletion(number, customer) == false);
-		return number;
+		customer.getBankAccount().setBalance(customer.getBankAccount().getBalance() - sumPayment);
+		salesman.getBankAccount().setBalance(salesman.getBankAccount().getBalance() + sumPayment);
 	}
 }
