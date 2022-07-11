dd6f99d68a7ec117ceacf2b3b121d8b8b3596ee2
Alexey <koroza.alexey99@gmail.com>
2022-07-11 18:22:21 +0200

    *

--- basics_oop/src/by/koroza/basics_oop/numberthree/entity/Day.java
+++ basics_oop/src/by/koroza/basics_oop/numberthree/entity/Day.java
@@ -4,9 +4,11 @@ public class Day {
 	private int number;
 	private boolean weekend;
 	private boolean holiday;
-	
+
 	public Day(int number, boolean weekend, boolean holiday) {
-		
+		this.number = number;
+		this.weekend = weekend;
+		this.holiday = holiday;
 	}
 
 	public int getNumber() {
@@ -33,4 +35,43 @@ public class Day {
 		this.weekend = weekend;
 	}
 
+	@Override
+	public int hashCode() {
+		int result = 31;
+		int prime = 1;
+		result = result * prime + number;
+		result = result * prime + Boolean.hashCode(weekend);
+		result = result * prime + Boolean.hashCode(holiday);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+		if (object == null) {
+			return false;
+		}
+		if (!getClass().equals(object.getClass())) {
+			return false;
+		}
+		Day day = (Day) object;
+		if (number != day.number) {
+			return false;
+		}
+		if (weekend != day.weekend) {
+			return false;
+		}
+		if (holiday != day.holiday) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() { // TODO toString()
+		StringBuilder builder = new StringBuilder();
+		return builder.toString();
+	}
 }
--- basics_oop/src/by/koroza/basics_oop/numberthree/entity/Month.java
+++ basics_oop/src/by/koroza/basics_oop/numberthree/entity/Month.java
@@ -24,4 +24,48 @@ public class Month {
 	public void setDays(Day[] days) {
 		this.days = days;
 	}
+
+	@Override
+	public int hashCode() {
+		int result = 31;
+		int prime = 1;
+		result = result * prime + (name != null ? name.hashCode() : 1);
+		result = result * prime + (days != null ? days.hashCode() : 1);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+		if (object == null) {
+			return false;
+		}
+		if (!getClass().equals(object.getClass())) {
+			return false;
+		}
+		Month month = (Month) object;
+		if (name == null) {
+			if (month.name != null) {
+				return false;
+			}
+		} else if (!name.equals(month.name)) {
+			return false;
+		}
+		if (days == null) {
+			if (month.days != null) {
+				return false;
+			}
+		} else if (!days.equals(month.days)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() { //TODO toString()
+		StringBuilder builder = new StringBuilder();
+		return builder.toString();
+	}
 }
