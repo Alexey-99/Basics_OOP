1b23010526b3131435f49b00ca47b7bccb59e348
Alexey <koroza.alexey99@gmail.com>
2022-07-11 18:49:04 +0200

    *

--- basics_oop/src/by/koroza/basics_oop/numberthree/main/Main.java
+++ basics_oop/src/by/koroza/basics_oop/numberthree/main/Main.java
@@ -4,11 +4,15 @@
 
 package by.koroza.basics_oop.numberthree.main;
 
+//import java.time.Year;
+
 import by.koroza.basics_oop.numberthree.entity.Сalendar;
+import by.koroza.basics_oop.numberthree.entity.Year;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Сalendar calendar = new Сalendar();
+		calendar.addYear(new Year(2022));
 	}
 }
