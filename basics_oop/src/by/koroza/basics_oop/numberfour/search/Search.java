589034155b941c9353ff27296c1f9d87721c4786
Alexey <koroza.alexey99@gmail.com>
2022-08-22 16:44:45 +0200

    *

--- basics_oop/src/by/koroza/basics_oop/numberfour/search/Search.java
+++ basics_oop/src/by/koroza/basics_oop/numberfour/search/Search.java
@@ -10,7 +10,21 @@ public class Search {
 
 	public void searchTreasureWithMinPrice(File file) throws FileNotFoundException {
 		double minPrice = findMinPrice(file);
-		printTreasureWithMinPrice(file, minPrice);
+		printTreasureWithSelectedPrice(file, minPrice);
+	}
+
+	public void searchTreasureWithMaxPrice(File file) throws FileNotFoundException {
+		double maxPrice = findMaxPrice(file);
+		printTreasureWithSelectedPrice(file, maxPrice);
+	}
+
+	public void searchTreasureBetweenMinToMaxPrices(File file, double minPrice, double maxPrice)
+			throws FileNotFoundException {
+		printTreasureBetweenMinToMaxPrices(file, minPrice, maxPrice);
+	}
+
+	public void searchTreasureSelectedPrice(File file, double price) throws FileNotFoundException {
+		printTreasureWithSelectedPrice(file, price);
 	}
 
 	private double findMinPrice(File file) throws FileNotFoundException {
@@ -30,18 +44,53 @@ public class Search {
 				price = priceBuffer;
 			}
 		}
-		System.out.println(minPrice);
+		System.out.println("MIN:" + minPrice);
 		return minPrice;
 	}
 
-	private void printTreasureWithMinPrice(File file, double minPrice) throws FileNotFoundException {
+	private double findMaxPrice(File file) throws FileNotFoundException {
+		double maxPrice = 0;
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(file);
 		while (scan.hasNextLine()) {
 			String line = scan.nextLine();
 			String[] array = line.split(LINE_TWO_SPACE);
+			if (array[0].equals(LINE_NUMBER_FIRST_LINE)) {
+				maxPrice = Double.parseDouble(array[2]);
+			}
 			double price = Double.parseDouble(array[2]);
-			if (price == minPrice) {
+			if (maxPrice < price) {
+				double priceBuffer = maxPrice;
+				maxPrice = price;
+				price = priceBuffer;
+			}
+		}
+		System.out.println("MAX:" + maxPrice);
+		return maxPrice;
+	}
+
+	private void printTreasureWithSelectedPrice(File file, double price) throws FileNotFoundException {
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(file);
+		while (scan.hasNextLine()) {
+			String line = scan.nextLine();
+			String[] array = line.split(LINE_TWO_SPACE);
+			double priceTreasure = Double.parseDouble(array[2]);
+			if (priceTreasure == price) {
+				System.out.println(line);
+			}
+		}
+	}
+
+	private void printTreasureBetweenMinToMaxPrices(File file, double minPrice, double maxPrice)
+			throws FileNotFoundException {
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(file);
+		while (scan.hasNextLine()) {
+			String line = scan.nextLine();
+			String[] array = line.split(LINE_TWO_SPACE);
+			double priceTreasure = Double.parseDouble(array[2]);
+			if (priceTreasure >= minPrice && priceTreasure <= maxPrice) {
 				System.out.println(line);
 			}
 		}
