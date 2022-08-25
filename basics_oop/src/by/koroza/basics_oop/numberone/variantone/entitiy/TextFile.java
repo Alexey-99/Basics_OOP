bd37e5d4dc49811147b14cfb2a17f0c1f55bc2e0
Alexey <koroza.alexey99@gmail.com>
2022-08-25 17:28:59 +0200

    *

--- basics_oop/src/by/koroza/basics_oop/numberone/variantone/entitiy/TextFile.java
+++ basics_oop/src/by/koroza/basics_oop/numberone/variantone/entitiy/TextFile.java
@@ -1,5 +1,31 @@
 package by.koroza.basics_oop.numberone.variantone.entitiy;
 
+import java.io.IOException;
+
 public class TextFile {
-	
+	private Directory directory;
+	private File file;
+
+	public TextFile(Directory directory, File file) throws IOException {
+		this.directory = directory;
+		this.directory.mkdirs();
+		this.file = file;
+		this.file.createNewFile();
+	}
+
+	public Directory getDirectory() {
+		return directory;
+	}
+
+	public void setDirectory(Directory directory) {
+		this.directory = directory;
+	}
+
+	public File getFile() {
+		return file;
+	}
+
+	public void setFile(File file) {
+		this.file = file;
+	}
 }
--- basics_oop/src/by/koroza/basics_oop/numberone/variantone/main/Main.java
+++ basics_oop/src/by/koroza/basics_oop/numberone/variantone/main/Main.java
@@ -10,14 +10,18 @@
 
 package by.koroza.basics_oop.numberone.variantone.main;
 
+import java.io.IOException;
+
 import by.koroza.basics_oop.numberone.variantone.entitiy.Directory;
 import by.koroza.basics_oop.numberone.variantone.entitiy.File;
+import by.koroza.basics_oop.numberone.variantone.entitiy.TextFile;
 
 public class Main {
 
-	public static void main(String[] args) { //TODO ПЕРЕДЕЛАТЬ
-		Directory directoryMain = new Directory("resouces");
+	public static void main(String[] args) throws IOException { // TODO ПЕРЕДЕЛАТЬ
+		Directory directoryMain = new Directory("resources");
 		Directory directoryFile = new Directory(directoryMain, "numberOne");
 		File file = new File(directoryFile, "textFile.txt");
+		TextFile textFile = new TextFile(directoryFile, file);
 	}
 }
