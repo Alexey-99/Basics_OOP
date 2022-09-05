40d89926c3f0f2efd4edc0c49f884cf38ae1ed69
Alexey <koroza.alexey99@gmail.com>
2022-09-05 16:26:17 +0200

    *

--- basics_oop/src/by/koroza/basics_oop/numberone/varianttwo/entitiy/Directoire.java
+++
@@ -1,9 +0,0 @@
-package by.koroza.basics_oop.numberone.varianttwo.entitiy;
-
-@SuppressWarnings("serial")
-public class Directoire extends File {
-	
-	public Directoire(String pathname) {
-		super(pathname);
-	}
-}
