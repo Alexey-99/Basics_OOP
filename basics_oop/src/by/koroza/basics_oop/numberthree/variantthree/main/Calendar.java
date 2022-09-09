9f83ab9f2ea6acfa0b0a245e6899528e5d9327ac
Alexey <koroza.alexey99@gmail.com>
2022-09-09 18:07:33 +0200

    *

--- basics_oop/src/by/koroza/basics_oop/numberthree/variantthree/main/Calendar.java
+++ basics_oop/src/by/koroza/basics_oop/numberthree/variantthree/main/Calendar.java
@@ -1,5 +1,10 @@
 package by.koroza.basics_oop.numberthree.variantthree.main;
 
 public class Calendar {
+	//private List[Date] holidays;
+	
+	public Calendar() {
+		
+	}
 
 }
--- basics_oop/src/by/koroza/basics_oop/numberthree/variantthree/main/Main.java
+++ basics_oop/src/by/koroza/basics_oop/numberthree/variantthree/main/Main.java
@@ -1,8 +1,17 @@
 package by.koroza.basics_oop.numberthree.variantthree.main;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		GregorianCalendar cannes = new GregorianCalendar(216, Calendar.AUGUST, 2);
+		cannes.set(Calendar.ERA, GregorianCalendar.BC);
+
+		DateFormat df = new SimpleDateFormat("dd MMM yyy GG");
+		System.out.println(df.format(cannes.getTime()));
 	}
 }
