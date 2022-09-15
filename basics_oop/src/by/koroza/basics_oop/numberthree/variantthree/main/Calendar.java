5dd240fcdf17e3f3107bbcc1776964ed0d5b9741
Alexey <koroza.alexey99@gmail.com>
2022-09-15 12:10:32 +0200

    *

--- basics_oop/src/by/koroza/basics_oop/numberthree/variantthree/main/Calendar.java
+++ basics_oop/src/by/koroza/basics_oop/numberthree/variantthree/main/Calendar.java
@@ -1,14 +1,17 @@
 package by.koroza.basics_oop.numberthree.variantthree.main;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Calendar {
 	private List<LocalDate> holidays;
+	private Validation validation;
 
 	public Calendar() {
 		this.holidays = new ArrayList<LocalDate>();
+		this.validation = new Validation();
 	}
 
 	public List<LocalDate> getHolidays() {
@@ -24,13 +27,32 @@ public class Calendar {
 	}
 
 	public void printWeekendsAndHolidays(LocalDate dateStart, LocalDate dateFinish) {
-		for (LocalDate i = dateStart; !i.isAfter(dateFinish); i = i.plusDays(1)) {
+		for (LocalDate day = dateStart; !day.isAfter(dateFinish); day = day.plusDays(1)) {
+			if ((this.validation.isHoliday(day) == true) || this.validation.isWeekend(day) == true) {
 
+			}
 		}
 	}
 
-	private class Validation{
-		
-		public
+	private class Validation {
+
+		public boolean isWeekend(LocalDate date) {
+			boolean isWeekend = false;
+			DayOfWeek dayOfWeek = date.getDayOfWeek();
+			if (dayOfWeek == DayOfWeek.SATURDAY) {
+				isWeekend = true;
+			}
+			return isWeekend;
+		}
+
+		public boolean isHoliday(LocalDate date) {
+			boolean isHoliday = false;
+			for (LocalDate day : holidays) {
+				if (date.isEqual(day)) {
+					isHoliday = true;
+				}
+			}
+			return isHoliday;
+		}
 	}
 }
