8ef35127185e011a489b60b1f60564b80a6d206b
Alexey <koroza.alexey99@gmail.com>
2022-09-18 15:48:12 +0200

    *

--- basics_oop/src/by/koroza/basics_oop/numberfive/flowercomposition/entity/flower/Flower.java
+++ basics_oop/src/by/koroza/basics_oop/numberfive/flowercomposition/entity/flower/Flower.java
@@ -1,6 +1,76 @@
 package by.koroza.basics_oop.numberfive.flowercomposition.entity.flower;
 
 public class Flower {
-	
+	private String groupName;
+	private String name;
+	private double price;
+	private static final String LINE_NAME = "Name: ";
+	private static final String LINE_PRICE = "Price: ";
 
+	public Flower(String groupName, String name, double price) {
+		this.groupName = groupName;
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getGroupName() {
+		return groupName;
+	}
+
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+
+	@Override
+	public int hashCode() {
+		int prime = 1;
+		int result = 31;
+		result = result * prime + (name != null ? name.hashCode() : 1);
+		result = result * prime + Double.hashCode(price);
+		result = result * prime + (LINE_NAME != null ? LINE_NAME.hashCode() : 1);
+		result = result * prime + (LINE_PRICE != null ? LINE_PRICE.hashCode() : 1);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+		if (object == null) {
+			return false;
+		}
+		if (!getClass().equals(object.getClass())) {
+			return false;
+		}
+		Flower flower = (Flower) object;
+		if (price != flower.price) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(LINE_NAME).append(name);
+		builder.append(LINE_PRICE).append(price);
+		return builder.toString();
+	}
 }
--- basics_oop/src/by/koroza/basics_oop/numberfive/flowercomposition/main/programlogic/Bouquet.java
+++ basics_oop/src/by/koroza/basics_oop/numberfive/flowercomposition/main/programlogic/Bouquet.java
@@ -48,28 +48,6 @@ public class Bouquet {
 	}
 
 	private static void addFlower(int number) {
-		switch(number) {
-		case 0:
-			
-			break;
-		case 1:
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		case 4:
-			break;
-		case 5: 
-			break;
-		case 6:
-			break;
-		case 7:
-			break;
-		case 8:
-			break;
-		case 9:
-			break;
-		}
+//		flowers.add();
 	}
 }
