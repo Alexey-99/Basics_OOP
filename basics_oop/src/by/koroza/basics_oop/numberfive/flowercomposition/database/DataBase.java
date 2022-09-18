c1a567de6763fa26b5fe6374cbcf85579afc6f7e
Alexey <koroza.alexey99@gmail.com>
2022-09-18 18:17:12 +0200

    *

--- basics_oop/src/by/koroza/basics_oop/numberfive/flowercomposition/database/DataBase.java
+++ basics_oop/src/by/koroza/basics_oop/numberfive/flowercomposition/database/DataBase.java
@@ -8,43 +8,8 @@ import by.koroza.basics_oop.numberfive.flowercomposition.entity.packaging.Packag
 
 
 public class DataBase {
-	// private static List<String> flowerNames = new ArrayList<>();
 	private static List<Flower> flowers = new ArrayList<>();
 	private static List<Packaging> packagings = new ArrayList<>();
-
-//	public static List<String> getFlowerNames() {
-//		return flowerNames;
-//	}
-//
-//	public static void setFlowerNames(List<String> flowerNames) {
-//		DataBase.flowerNames = flowerNames;
-//	}
-
-//	public static List<String> getPackagingTypes() {
-//		return packagingTypes;
-//	}
-//
-//	public static void setPackagingTypes(List<String> packagingTypes) {
-//		DataBase.packagingTypes = packagingTypes;
-//	}
-
-//	public static void addFlowerName(String name) {
-//		DataBase.flowerNames.add(name);
-//	}
-
-//	public static void addpackaging(String type) {
-//		DataBase.packagings.add(type);
-//	}
-
-//	public static void printFlowerNames() {
-//		for (int i = 0; i < flowerNames.size(); i++) {
-//			System.out.println(i + " - " + flowerNames.get(i));
-//		}
-//	}
-
-	//
-
-	//
 
 	public static List<Flower> getFlowers() {
 		return flowers;
