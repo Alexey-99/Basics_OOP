2d1eb6f843ec6eb39047b87d2b002889510e95bb
Alexey <koroza.alexey99@gmail.com>
2022-09-18 17:50:34 +0200

    *

--- basics_oop/src/by/koroza/basics_oop/numberfive/flowercomposition/entity/packaging/Packeging.java
+++ basics_oop/src/by/koroza/basics_oop/numberfive/flowercomposition/entity/packaging/Packeging.java
@@ -1,16 +1,13 @@
-package by.koroza.basics_oop.numberfive.flowercomposition.entity.flower;
+package by.koroza.basics_oop.numberfive.flowercomposition.entity.packaging;
 
-public class Flower {
-	private String groupName;
+public class Packeging {
 	private String name;
 	private double price;
-	private static final String LINE_GROUP_FLOWER = "Group of flower: ";
 	private static final String LINE_NAME = "Name: ";
 	private static final String LINE_PRICE = "Price: ";
 	private static final String OPERATION_NEXT_LINE = "\n";
 
-	public Flower(String groupName, String name, double price) {
-		this.groupName = groupName;
+	public Packeging(String name, double price) {
 		this.name = name;
 		this.price = price;
 	}
@@ -31,24 +28,14 @@ public class Flower {
 		this.price = price;
 	}
 
-	public String getGroupName() {
-		return groupName;
-	}
-
-	public void setGroupName(String groupName) {
-		this.groupName = groupName;
-	}
-
 	@Override
 	public int hashCode() {
 		int prime = 1;
 		int result = 31;
-		result = result * prime + (groupName != null ? groupName.hashCode() : 1);
 		result = result * prime + (name != null ? name.hashCode() : 1);
 		result = result * prime + Double.hashCode(price);
 		result = result * prime + (LINE_NAME != null ? LINE_NAME.hashCode() : 1);
 		result = result * prime + (LINE_PRICE != null ? LINE_PRICE.hashCode() : 1);
-		result = result * prime + (LINE_GROUP_FLOWER != null ? LINE_GROUP_FLOWER.hashCode() : 1);
 		result = result * prime + (OPERATION_NEXT_LINE != null ? OPERATION_NEXT_LINE.hashCode() : 1);
 		return result;
 	}
@@ -64,22 +51,15 @@ public class Flower {
 		if (!getClass().equals(object.getClass())) {
 			return false;
 		}
-		Flower flower = (Flower) object;
-		if (groupName == null) {
-			if (flower.groupName != null) {
-				return false;
-			}
-		} else if (!groupName.equals(flower.groupName)) {
-			return false;
-		}
+		Packeging packeging = (Packeging) object;
 		if (name == null) {
-			if (flower.name != null) {
+			if (packeging.name != null) {
 				return false;
 			}
-		} else if (!name.equals(flower.name)) {
+		} else if (!name.equals(packeging.name)) {
 			return false;
 		}
-		if (price != flower.price) {
+		if (price != packeging.price) {
 			return false;
 		}
 		return true;
@@ -88,7 +68,6 @@ public class Flower {
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		builder.append(LINE_GROUP_FLOWER).append(groupName).append(OPERATION_NEXT_LINE);
 		builder.append(LINE_NAME).append(name).append(OPERATION_NEXT_LINE);
 		builder.append(LINE_PRICE).append(price);
 		return builder.toString();
