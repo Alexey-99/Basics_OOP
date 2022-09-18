612fc3ff6bcde88ec5b89922ff33fc50cc040138
Alexey <koroza.alexey99@gmail.com>
2022-09-18 19:21:02 +0200

    *

--- basics_oop/src/by/koroza/basics_oop/numberfive/flowercomposition/main/programlogic/FlowerComposition.java
+++ basics_oop/src/by/koroza/basics_oop/numberfive/flowercomposition/main/programlogic/FlowerComposition.java
@@ -1,9 +1,15 @@
 package by.koroza.basics_oop.numberfive.flowercomposition.main.programlogic;
 
+import java.util.List;
+
+import by.koroza.basics_oop.numberfive.flowercomposition.entity.flower.Flower;
+import by.koroza.basics_oop.numberfive.flowercomposition.entity.packaging.Packaging;
+
 public class FlowerComposition {
-	
+
 	public static void createFlowerComposition() {
-		
+		List<Flower> flowers = CreateBouquet.selectionFlowers();
+		Packaging packaging = SelectionPackaging.selectionFlowers();
 	}
 
 }
